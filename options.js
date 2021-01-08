1be0b46540aafa16a01574860b3da86541d8b0f3
ravindrayadavrk1006 <ravindrayadavrk1006@gmail.com>
2021-01-08 13:31:11 +0530

    for heroku

--- options.js
+++ options.js
@@ -1,4 +1,4 @@
 var fs = require('fs'),
-configPath = './configuration.json';
+configPath = './configuration1.json';
 var parsed = JSON.parse(fs.readFileSync(configPath, 'UTF-8'));
 exports.mail=  parsed;
